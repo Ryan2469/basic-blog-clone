454751b06095776a4c8aa5e17e987f887a76c9d1
Ryan2469 <amg9981@naver.com>
2024-03-09 15:00:00 +0900

    posts, header, components ui

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,19 +1,27 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "../pages/home";
+import PostList from "../pages/posts";
+import PostDetail from "../pages/posts/detail";
+import PostNew from "../pages/posts/new";
+import PostEdit from "../pages/posts/edit";
+import ProfilePage from "../pages/profile";
+import LoginPage from "../pages/login";
+import SignupPage from "../pages/signup";
 
-import { Route, Routes, Navigate} from 'react-router-dom';
-import Home from '../pages/Home';
-
-const Router = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/posts' element={<h1>Posts Page</h1>} />
-            <Route path='/posts/:id' element={<h1>Posts Detail Page</h1>} />
-            <Route path='/posts/new' element={<h1>Posts New Page</h1>} />
-            <Route path='/posts/edit/:id' element={<h1>Posts Edit Page</h1>} />
-            <Route path='/profile' element={<h1>Profile Page</h1>} />
-            <Route path='/*' element={<Navigate replace to='/'/>} />
-        </Routes>
-    )
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts/new" element={<PostNew />} />
+        <Route path="/posts/edit/:id" element={<PostEdit />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
+  );
 }
-
-export default Router;
