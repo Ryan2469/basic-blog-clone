a6a29e8e90e9c839af7a1a27ab7ffa21f035637d
Ryan2469 <amg9981@naver.com>
2024-03-18 22:08:35 +0900

    firebase 로그인 설정 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Router from 'components/Router';
+import { app } from "firebaseApp";
+import { getAuth } from "firebase/auth";
 
 function App() {
+  
+  const auth = getAuth(app);
+  console.log(auth, "auth");
+
+  const [ isAuthenticated, setIsAuthenticated ] = useState<boolean>(!!auth?.currentUser);
+
+
   return (
-      <Router />
+      <Router isAuthenticated={isAuthenticated}/>
   );
 }
 
