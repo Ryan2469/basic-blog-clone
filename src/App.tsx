43559a12b3c45c44ea0fdcc8c8dedea9d3e33dd7
Ryan54545 <bluewhale.ryan2469@gmail.com>
2024-03-20 09:56:55 +0900

    Revert "firebase 로그인, 로그아웃 구현완료"
    This reverts commit 4648f326bd034826c3d49c3b1cc0a5d0b615faa4.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Router from 'components/Router';
 import { app } from "firebaseApp";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Loader from 'components/Loader';
 
 function App() {
   
   const auth = getAuth(app);
-  const [init, setInit] = useState<boolean>(false);
+  console.log(auth, "auth");
+
   const [ isAuthenticated, setIsAuthenticated ] = useState<boolean>(!!auth?.currentUser);
-  
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      console.log(auth, "auth", user);
-      if(user){
-        setIsAuthenticated(true);
-      } else {
-        setIsAuthenticated(false);
-      }
-      setInit(true);
-    })
-  })
+
 
   return (
     <>
+      <Router isAuthenticated={isAuthenticated}/>
       <ToastContainer />
-      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,34 +1,18 @@
-import { getAuth, signOut } from "firebase/auth";
-import { app } from "firebaseApp";
 import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
 
 export default function Profile() {
-
-  const auth = getAuth(app);
-
-  const onSignOut = async() => {
-    try {
-      const auth = getAuth(app);
-      await signOut(auth);
-      toast.success('로그아웃 됐습니다.')
-    } catch (error:any) {
-      toast.error(error?.code);
-    }
-  }
-
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">{auth?.currentUser?.displayName || "사용자"}</div>
+          <div className="profile__email">test@test.com</div>
+          <div className="profile__name">김유저</div>
         </div>
       </div>
-      <div role='presentation' className="profile__logout" onClick={onSignOut}>
+      <Link to="/" className="profile__logout">
         로그아웃
-      </div>
+      </Link>
     </div>
   );
 }
